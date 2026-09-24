24b7252934a5de99d65a4dbf232d2d899b3b97f3
maestroplaya <maestroplaya@duck.com>
2026-09-25 02:45:18 +0300

    fix(script): reordered imports per PEP8

--- filter_schedule.py
+++ filter_schedule.py
@@ -1,8 +1,9 @@
 import os
 import re
-from icalendar.cal.calendar import Component
+
 import requests
-from icalendar import Calendar, Event
+from icalendar import Calendar
+from icalendar.cal.calendar import Component
 
 SOURCE_URL = "https://p.inh.tatar/music-room/v0/music-room.ics"
 OUTPUT_DIR = "dist"
